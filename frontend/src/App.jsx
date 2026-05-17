84cd20570a3358061e60a0c6e489571bf8756a6e
Shreeya <srivastavshreeya0@gmail.com>
2026-05-17 20:23:55 +0530

    fix frontend changessss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LandingPage  from "./pages/LandingPage";
 import LoginPage    from "./pages/LoginPage";
 import SignupPage   from "./pages/SignupPage";
@@ -6,13 +6,15 @@ import Dashboard    from "./pages/Dashboard";
 import SharedNote   from "./pages/SharedNote";
 
 function App() {
+  const token = localStorage.getItem("token");
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/"                  element={<LandingPage />} />
-        <Route path="/login"             element={<LoginPage />} />
-        <Route path="/signup"            element={<SignupPage />} />
-        <Route path="/dashboard"         element={<Dashboard />} />
+        <Route path="/"                  element={token ? <Navigate to="/dashboard" replace /> : <LandingPage />} />
+        <Route path="/login"             element={token ? <Navigate to="/dashboard" replace /> : <LoginPage />} />
+        <Route path="/signup"            element={token ? <Navigate to="/dashboard" replace /> : <SignupPage />} />
+        <Route path="/dashboard"         element={token ? <Dashboard /> : <Navigate to="/login" replace />} />
         <Route path="/shared/:shareId"   element={<SharedNote />} />
       </Routes>
     </BrowserRouter>
